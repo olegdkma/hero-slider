f4fbb6c364cc9e80d67dee912df2cfb815952e7d
oleh <oleg.duldier@gmail.com>
2020-10-27 14:03:26 +0200

    push

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,60 @@
-// Main js file
-// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+import {tns} from 'tiny-slider/src/tiny-slider';
+import $ from 'jquery';
+let mainSlider = document.getElementById('hero-slider')
+//let secSlider = document.getElementById('hero-slider')
 
-console.log('its work')
+  const slider = tns({
+    container: '#main__slider',
+    items: 1,
+    //slideBy: 'page',
+    autoplay: false,
+    autoplayButtonOutput: false,
+    nav: true,
+    navContainer: '.slider__count',
+    controls: false,
+    stop: false,
+    autoplayTimeout: 5000,
+    mouseDrag: true,
+
+  });
+const slider2 = tns({
+  container: '#main__slider2',
+  items: 1,
+  //slideBy: 'page',
+  autoplay: false,
+  autoplayButtonOutput: false,
+  nav: true,
+  navContainer: '.slider__count2',
+  controls: false,
+  stop: false,
+  autoplayTimeout: 5000,
+  mouseDrag: true,
+
+});
+
+if(mainSlider){
+  const slider = tns({
+    container: '#hero-slider',
+    items: 1,
+    nav: false,
+    controls: true,
+    mouseDrag: true,
+    controlsContainer:'.controls',
+    prevButton: '.controls__button_prev',
+    nextButton: '.controls__button_next'
+
+  });
+}
+$(document).ready(function (){
+    $('#icon2').on('click', function (){
+      $(this).toggleClass('open')
+      $('header').toggleClass('active')
+      $('.menu').slideToggle()
+      let windowWidth =  window.innerWidth
+      console.log(windowWidth < 992,windowWidth)
+      if(windowWidth < 992){
+
+        $('.lang').slideToggle()
+      }
+    })
+})
